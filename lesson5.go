22077485c0b0b278b3a905f43b95194f848c6b4c
SatoruItaya <44042909+SatoruItaya@users.noreply.github.com>
2019-10-12 23:22:31 +0900

    Update lesson5.go

--- lesson5.go
+++ lesson5.go
@@ -9,11 +9,11 @@ func main() {
 
 	fmt.Println("Spaceline        Days Trip type    Price")
 	fmt.Println("===============================================")
-	
+
 	distance := 62100000
 	spaceline := ""
 	tirpType := ""
-	
+
 	for i := 0; i <= 10; i++ {
 		switch rand.Intn(3) {
 		case 0:
@@ -23,18 +23,18 @@ func main() {
 		case 2:
 			spaceline = "Virgin Galactic"
 		}
-		
+
 		velocity := rand.Intn(15) + 16
-		days := distance/velocity/60/60/24
+		days := distance / velocity / 60 / 60 / 24
 		price := velocity + 20
 
 		if rand.Intn(2) == 0 {
 			tirpType = "One-way"
 		} else {
-			tirpType="Round-trip"
+			tirpType = "Round-trip"
 			price *= 2
 		}
-		
+
 		fmt.Printf("%-16v %-3v  %-12v $%3v\n", spaceline, days, tirpType, price)
 	}
 }
