16dc85d271eff2bef47bb2d3fb95c95595db2de5
SatoruItaya <str.itaya@gmail.com>
2019-10-22 14:48:01 +0900

    finish lesson15

---
+++ lesson15/tables.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+)
+	
+type celsius float64
+type fahrenheit float64
+type getRowFn func(row int) (string, string)
+
+const (
+	line = "========================"
+	rowFormat = "| %8s | %8s |\n"
+	numberFormat = "%.1f"
+)
+
+func (c celsius) fahrenheit() fahrenheit{
+	return fahrenheit((c * 9.0 / 5.0) + 32.0)
+}
+
+func (f fahrenheit) celsius() celsius {
+	return celsius((f - 32.0) * 5.0 /9.0)
+}
+
+func drawTable(hdr1, hdr2 string, rows int, getRow getRowFn){
+	fmt.Println(line)
+	fmt.Printf(rowFormat, hdr1, hdr2)
+	fmt.Println(line)
+	for row := 0; row < rows; row++{
+		cell1, cell2 := getRow(row)
+		fmt.Printf(rowFormat, cell1, cell2)
+	}
+	fmt.Println(line)
+}
+
+func cTof(row int) (string, string){
+	c := celsius(row * 5 -40)
+	f := c.fahrenheit()
+	cell1 := fmt.Sprintf(numberFormat, c)
+	cell2 := fmt.Sprintf(numberFormat, f)
+	return cell1, cell2
+}
+
+func fToc(row int) (string, string){
+	f := fahrenheit(row * 5 -40)
+	c := f.celsius()
+	cell1 := fmt.Sprintf(numberFormat, f)
+	cell2 := fmt.Sprintf(numberFormat, c)
+	return cell1, cell2
+}
+
+
+func main() {
+	drawTable("℃", "℉", 29, cTof)
+	fmt.Println()
+	drawTable("℉","℃", 29, fToc)
+}
